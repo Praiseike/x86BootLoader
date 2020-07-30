8b8974e491bac3f19db1e34a918fa9795a08c3d1
Praiseike <praiseike123@gmail.com>
2020-07-30 21:17:48 +0100

    accepted pull request

---
+++ kernel/mem.c
@@ -0,0 +1,34 @@
+
+
+void * memcopy(char * dest,char * source,int count)
+{
+	int i = count;
+	while(i < count)
+	{
+		dest[i] =  source[i];
+		i++;
+	}
+
+	return dest;
+}
+
+void * memset(char * s,int c,int count)
+{
+	int i  = count;
+	while (i < count)
+	{
+		s[i] = c;
+		i++;
+	}
+	return s;
+}
+
+int strlen(const char * string)
+{
+    int count = 0;
+    while(*string++)
+    {
+        count++;
+    }
+    return count;
+}
